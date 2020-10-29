4b00a8015e23496ae5357cdd2f2c25b9bdb24c33
Dhruv <dhruvupadhyay88@gmail.com>
2020-10-29 19:44:47 -0400

    some parts working

---
+++ client/build/precache-manifest.2887d7473c80edcaef7102fcdbb373f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdaaa350c99385d4c3499c4994b483e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c6b067d3b389b7f2ec8",
+    "url": "/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "3c6b067d3b389b7f2ec8",
+    "url": "/static/js/2.6f4824a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6f4824a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63447cb6ab902251e3c",
+    "url": "/static/js/main.544f020c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "36f90297ed797e3310aa347aea8eb492",
+    "url": "/static/media/loginbg1.36f90297.jpg"
+  },
+  {
+    "revision": "c2e472f110e1c04e89e9df19ffd957aa",
+    "url": "/static/media/topartist.c2e472f1.jpg"
+  },
+  {
+    "revision": "43a7ebbcd70c899c080d01b912b07e34",
+    "url": "/static/media/toptracks.43a7ebbc.jpg"
+  }
+]);
---
+++ oldBuild/precache-manifest.5b13dccb778ca3811b6b0ae21f7c6855.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14713fc3cfd092c745913b68d3e7e1ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c6b067d3b389b7f2ec8",
+    "url": "/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "3c6b067d3b389b7f2ec8",
+    "url": "/static/js/2.6f4824a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6f4824a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9af4e8cf9c24dd32b9f",
+    "url": "/static/js/main.47efe70b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "36f90297ed797e3310aa347aea8eb492",
+    "url": "/static/media/loginbg1.36f90297.jpg"
+  },
+  {
+    "revision": "c2e472f110e1c04e89e9df19ffd957aa",
+    "url": "/static/media/topartist.c2e472f1.jpg"
+  },
+  {
+    "revision": "43a7ebbcd70c899c080d01b912b07e34",
+    "url": "/static/media/toptracks.43a7ebbc.jpg"
+  }
+]);
